d290be9b3509fe6d96e83109f47dab1e22291a56
tmargate <margate.tristan@gmail.com>
2026-01-11 20:07:30 +0100

    update pdf lecture

--- src/clinical_cool_etud/config.py
+++ src/clinical_cool_etud/config.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 
-# 1. On récupère le chemin absolu du fichier config.py
-# 2. On remonte l'arborescence pour trouver la racine du projet
 # src/clinical_tool/config.py -> parent -> src -> parent -> racine
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 
@@ -10,6 +8,5 @@ DATA_DIR = PROJECT_ROOT / "data"
 RESULTS_DIR = PROJECT_ROOT / "results"
 
 # Optionnel : Création automatique des dossiers s'ils n'existent pas
-# Comme ça, tu n'as pas d'erreur "Directory not found"
 DATA_DIR.mkdir(parents=True, exist_ok=True)
 RESULTS_DIR.mkdir(parents=True, exist_ok=True)
